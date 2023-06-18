8d37cc41cbfe82d0206beb1d87a6a7723b434d3d
seokjin1013 <seokjin1013@gmail.com>
2023-06-18 16:12:58 +0900

    change control keys (#1)

--- main.py
+++ main.py
@@ -1,35 +1,34 @@
 import pygame, sys
+import math
 from random import randint
 
-
 class Tree(pygame.sprite.Sprite):
-	def __init__(self,pos,group):
+	def __init__(self, pos, group):
 		super().__init__(group)
-		self.image = pygame.image.load('./assets/tree.png').convert_alpha()
+		self.image = pygame.image.load('assets/tree.png').convert_alpha()
 		self.rect = self.image.get_rect(topleft = pos)
 
-
 class Player(pygame.sprite.Sprite):
-	def __init__(self,pos,group):
+	def __init__(self, pos, group):
 		super().__init__(group)
-		self.image = pygame.image.load('./assets/player.png').convert_alpha()
-		self.rect = self.image.get_rect(center = pos)
+		self.image = pygame.image.load('assets/player.png').convert_alpha()
+		self.rect = self.image.get_rect(center=pos)
 		self.direction = pygame.math.Vector2()
 		self.speed = 5
 
 	def input(self):
 		keys = pygame.key.get_pressed()
 
-		if keys[pygame.K_UP]:
+		if keys[pygame.K_w]:
 			self.direction.y = -1
-		elif keys[pygame.K_DOWN]:
+		elif keys[pygame.K_s]:
 			self.direction.y = 1
 		else:
 			self.direction.y = 0
 
-		if keys[pygame.K_RIGHT]:
+		if keys[pygame.K_d]:
 			self.direction.x = 1
-		elif keys[pygame.K_LEFT]:
+		elif keys[pygame.K_a]:
 			self.direction.x = -1
 		else:
 			self.direction.x = 0
@@ -38,15 +37,26 @@ class Player(pygame.sprite.Sprite):
 		self.input()
 		self.rect.center += self.direction * self.speed
 
-class Bullet(pygame.sprite.Sprite):
-	def __init__(self,pos,group):
-		super().__init__(group)
-		self.image = pygame.Surface((50,10))
-		self.image.fill((255,0,0))
-		self.rect = self.image.get_rect(center = pos)
-		self.direction = pygame.math.Vector2()
-		self.speed = 15
+	def create_bullet(self, camera_pos, camera_group):
+		return Bullet(self.rect, camera_pos, camera_group)
+    		
 
+class Bullet(pygame.sprite.Sprite):
+	def __init__(self, player_pos, camera_pos, group):
+		super().__init__(group)
+		self.pos = pygame.math.Vector2(player_pos.center)
+		self.image = pygame.Surface((10, 10))
+		self.image.fill((255, 0, 0))
+		self.rect = self.image.get_rect(center=self.pos)
+
+		self.speed = 15
+		self.vel = pygame.math.Vector2(pygame.mouse.get_pos())
+		self.vel += camera_pos - player_pos.center
+		self.vel = self.vel.normalize() * self.speed
+		
+	def update(self):
+		self.pos += self.vel
+		self.rect.center = self.pos
 
 class CameraGroup(pygame.sprite.Group):
 	def __init__(self):
@@ -64,10 +74,10 @@ class CameraGroup(pygame.sprite.Group):
 		t = self.camera_borders['top']
 		w = self.display_surface.get_size()[0]  - (self.camera_borders['left'] + self.camera_borders['right'])
 		h = self.display_surface.get_size()[1]  - (self.camera_borders['top'] + self.camera_borders['bottom'])
-		self.camera_rect = pygame.Rect(l,t,w,h)
+		self.camera_rect = pygame.Rect(l, t, w, h)
 
 		# ground
-		self.ground_surf = pygame.image.load('./assets/ground.png').convert_alpha()
+		self.ground_surf = pygame.image.load('assets/ground.png').convert_alpha()
 		self.ground_rect = self.ground_surf.get_rect(topleft = (0,0))
 
 		# camera speed
@@ -76,7 +86,7 @@ class CameraGroup(pygame.sprite.Group):
 
 		# zoom 
 		self.zoom_scale = 1
-		self.internal_surf_size = (2500,2500)
+		self.internal_surf_size = (2500, 2500)
 		self.internal_surf = pygame.Surface(self.internal_surf_size, pygame.SRCALPHA)
 		self.internal_rect = self.internal_surf.get_rect(center = (self.half_w,self.half_h))
 		self.internal_surface_size_vector = pygame.math.Vector2(self.internal_surf_size)
@@ -84,7 +94,7 @@ class CameraGroup(pygame.sprite.Group):
 		self.internal_offset.x = self.internal_surf_size[0] // 2 - self.half_w
 		self.internal_offset.y = self.internal_surf_size[1] // 2 - self.half_h
 
-	def center_target_camera(self,target):
+	def center_target_camera(self, target):
 		self.offset.x = target.rect.centerx - self.half_w
 		self.offset.y = target.rect.centery - self.half_h
 
@@ -95,7 +105,7 @@ class CameraGroup(pygame.sprite.Group):
 		if keys[pygame.K_e]:
 			self.zoom_scale -= 0.1
 
-	def custom_draw(self,player):
+	def custom_draw(self, player):
 		self.center_target_camera(player)
 		self.zoom_keyboard_control()
 
@@ -131,16 +141,17 @@ pygame.init()
 screen = pygame.display.set_mode((1280,720))
 clock = pygame.time.Clock()
 fps = FPS() 
+
 # setup 
 camera_group = CameraGroup()
-player = Player((640,360),camera_group)
+player = Player((640,360), camera_group)
 
 for i in range(20):
 	random_x = randint(0,1000)
 	random_y = randint(0,1000)
-	Tree((random_x,random_y),camera_group)
+	Tree((random_x,random_y), camera_group)
 
-font = pygame.font.SysFont('arial', 30, True, True)
+bullet_group = pygame.sprite.Group()
 
 while True:
 	for event in pygame.event.get():
@@ -151,16 +162,16 @@ while True:
 			if event.key == pygame.K_ESCAPE:
 				pygame.quit()
 				sys.exit()
-
+		if event.type == pygame.MOUSEBUTTONDOWN:
+			camera_group.add(player.create_bullet(camera_group.offset, camera_group))
+			
 		if event.type == pygame.MOUSEWHEEL:
 			camera_group.zoom_scale += event.y * 0.03
-		
-		#if event.type == pygame.MOUSEBUTTONDOWN:
 
 	screen.fill('#71ddee')
 	camera_group.update()
 	camera_group.custom_draw(player)
 	fps.render(screen)
 	pygame.display.update()
-	print(camera_group.offset)
+	print(camera_group.offset, camera_group.zoom_scale, pygame.mouse.get_pos())
 	fps.clock.tick(60)
